5b50bf571f0b1c31618767a8966ea77e566f9813
luishnrqq <luisaraujodemelo@hotmail.com>
2024-06-27 12:53:15 -0300

    consegui adcionar efeito de transicao, falta consertar alguns detalhes no css

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" >
-      <body className={inter.className} class="m-0 p-0">{children}</body>
+      <body className={inter.className} >{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,56 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [cadastroVisible, setCadastroVisible] = useState(false);
+
+  const handleCadastroClick = () =>{
+    setCadastroVisible(!cadastroVisible);
+  }
+
   return (
-    <main>
-      <div className="flex-container">
-        <div  className="left-side-login">
-          <h1>Bem vindo ao nosso site</h1>
-          <img src="Users\luisa\Downloads\10248741-removebg-preview.png"  />
+
+    <div className="flex-container">
+      <input type="checkbox"  id="chk"/>
+      <div className="left-side-login">
+        <h1>Bem vindo ao nosso site</h1>
+      </div>
+      <div className="right-login">
+        <h1 >LOGIN</h1>
+        <div className="info-input">
+          <label for="usuario">Usuário</label>
+          <input type="text" placeholder="Usuário" />
         </div>
-        <div className="right-login">
-          <h1>LOGIN</h1>
-          <div className="info-input">
-            <label  for="usuario">Usuário</label>
-            <input type="text" placeholder="Usuário" />
+        <div className="info-input">
+          <label for="senha"  >Senha</label>
+          <input type="password" placeholder="******" />
+        </div>
+        <button className="login-btn">Entrar</button>
+        <div className={cadastroVisible ? "cadstro-conatiner cadastro-container-active" : "cadastro-container"} >
+          <label htmlFor="chk" aria-hidden="true" onClick={handleCadastroClick} >CADASTRO</label>
+          <div className="info-input-register">
+            <label>Nome completo</label>
+            <input type="text" placeholder="Nome Completo" />
           </div>
-          <div className="info-input">
-            <label  for="senha"  >Senha</label>
+          <div className="info-input-register">
+            <label>Email</label>
+            <input type="email" placeholder="E-mail" />
+          </div>
+          <div className="info-input-register">
+            <label>Senha</label>
             <input type="password" placeholder="******" />
           </div>
-          <button className="login-btn">Entrar</button>
+          <div className="info-input-register">
+            <label>Confirmar senha</label>
+            <input type="password" placeholder="******" />
+          </div>
+          <div>
+            <button className="login-btn">Cadastrar</button>
+          </div>
         </div>
       </div>
-    </main>
+    </div>
+
   );
 }
+
